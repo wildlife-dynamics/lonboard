340964d2293fdf2fb2fca46af1a342f92ed89916
Alex Morling <atmorling@gmail.com>
2025-08-30 14:13:24 +1000

    update widget id type

--- lonboard/types/layer.py
+++ lonboard/types/layer.py
@@ -98,7 +98,7 @@ class BitmapLayerKwargs(BaseLayerKwargs, total=False):
 
 class BitmapTileLayerKwargs(BaseLayerKwargs, total=False):
     data: str | Sequence[str]
-    widget_id: int | None
+    widget_id: str | None
     tile_size: int
     zoom_offset: int
     max_zoom: int
